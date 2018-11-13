6692eacc395ed2f412055a61d75ccfd3926f6d9e
扫码改需求 <1043009478@qq.com>
2018-11-13 13:55:02 +0800

    修改数据库

--- apartmentmanager.sql
+++ apartmentmanager.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50540
 File Encoding         : 65001
 
-Date: 2018-10-25 12:42:54
+Date: 2018-11-13 13:54:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,13 +24,15 @@ CREATE TABLE `admin_info` (
   `userPwd` varchar(255) NOT NULL,
   `userType` varchar(255) NOT NULL,
   `isDel` bit(1) NOT NULL DEFAULT b'0',
+  `openid` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of admin_info
 -- ----------------------------
-INSERT INTO `admin_info` VALUES ('admin', 'admin', '超级管理员', '\0');
+INSERT INTO `admin_info` VALUES ('admin', 'admin', '超级管理员', '\0', '');
+INSERT INTO `admin_info` VALUES ('H18020017', '801220056', '超级管理员', '\0', '');
 
 -- ----------------------------
 -- Table structure for dormadmin_info
@@ -50,7 +52,8 @@ CREATE TABLE `dormadmin_info` (
 -- Records of dormadmin_info
 -- ----------------------------
 INSERT INTO `dormadmin_info` VALUES ('1001', '张三', '男', '13752707769', '\0');
-INSERT INTO `dormadmin_info` VALUES ('1002', '李四', '男', '12345678978', '\0');
+INSERT INTO `dormadmin_info` VALUES ('1002', '李四', '男', '15927665941', '\0');
+INSERT INTO `dormadmin_info` VALUES ('1003', '王五', '女', '1155648973456', '');
 
 -- ----------------------------
 -- Table structure for dorm_info
@@ -71,6 +74,10 @@ CREATE TABLE `dorm_info` (
 -- ----------------------------
 -- Records of dorm_info
 -- ----------------------------
+INSERT INTO `dorm_info` VALUES ('101', '男生宿舍', '5', '1002', '\0');
+INSERT INTO `dorm_info` VALUES ('102', '男生宿舍', '5', '1001', '\0');
+INSERT INTO `dorm_info` VALUES ('201', '女生宿舍', '4', '1003', '\0');
+INSERT INTO `dorm_info` VALUES ('202', '女生宿舍', '4', '1003', '\0');
 
 -- ----------------------------
 -- Table structure for student_info
@@ -94,3 +101,7 @@ CREATE TABLE `student_info` (
 -- ----------------------------
 -- Records of student_info
 -- ----------------------------
+INSERT INTO `student_info` VALUES ('123456789', 'lsw', '男', '22', '123465789', 'H1802', '101', '/public/upimages/ab325c13f05363cd9f78fdeec95c20a4.jpg', '\0');
+INSERT INTO `student_info` VALUES ('H18020017', '李嘉图', '男', '21', '15915915915', 'H1802', '101', '/public/upimages/a651be4b74737a09f0c0bd96b7e39573.jpg', '\0');
+INSERT INTO `student_info` VALUES ('T18020017', 'Ricardo', '男', '20', '13613613613', 'H1802', '102', '/public/upimages/a0fd6f066c72dbdfbace8fb2f3232b78.jpg', '\0');
+INSERT INTO `student_info` VALUES ('T18020018', '嘿嘿嘿', '男', '22', '17617617617', 'H1802', '102', '/public/upimages/3502329cbd69985b2f39744dabf2d789.jpg', '\0');
